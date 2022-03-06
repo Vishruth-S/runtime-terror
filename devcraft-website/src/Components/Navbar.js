89c70f5c9593a12c522ae682be1c656c6b4ff95f
Vishruth S <vs.vishruth.s@gmail.com>
2022-03-06 07:41:53 +0530

    :art: Home page set

--- devcraft-website/src/Components/Navbar.js
+++ devcraft-website/src/Components/Navbar.js
@@ -1,24 +1,53 @@
-import React from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../css/Navbar.css"
+import { auth } from "../firebase-config";
 function Navbar() {
+  const [user, setUser] = useState({});
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+  const logout = async () => {
+    await signOut(auth);
+  };
   return (
-    <div className="home-navbar">
-      <img
-        src="https://cdn.dribbble.com/users/24078/screenshots/15522433/media/e92e58ec9d338a234945ae3d3ffd5be3.jpg?compress=1&resize=400x300&vertical=top"
-        className="logo"
-        alt="logo"
-      />
-      <div className="home-navleft">
-        <button className=" btn home-login" type="button">
-          Login
-        </button>
+    <nav class="home-navbar navbar navbar-expand-lg">
+      <h1 class="navbar-brand" href="#">MaxMoney</h1>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <button className=" btn home-getstarted" type="button">
-          Get Started
-        </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          {/* <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li> */}
+        </ul>
+        {user && user.uid ?
+          <button className=" btn home-login" type="button" onClick={logout}>
+            Logout
+          </button>
+          : <div>
+            <Link to="/login">
+              <button className=" btn home-login" type="button">
+                Login
+              </button>
+            </Link>
+            <Link to="/register">
+              <button className=" btn home-getstarted" type="button">
+                Get Started
+              </button>
+            </Link>
+          </div>
+        }
       </div>
-    </div>
+    </nav>
   );
 }
 
 export default Navbar;
+
+
+
+
--- devcraft-website/src/pages/Home.js
+++ devcraft-website/src/pages/Home.js
@@ -1,44 +1,47 @@
 import React from 'react'
 import '../../src/css/Home.css'
 import Navbar from '../Components/Navbar'
+import img1 from '../assets/trust.png'
+import img2 from '../assets/shield.png'
+import img3 from '../assets/insurance.png'
 
 const Home = () => {
-    return (
-        <div>
-        <Navbar/>
-        <div className='homeinfo'>
-          <h1>Meet banking that keeps pace with you</h1>
-          <h3>Open a fully free digital bank account and revamp the way you bank</h3>
-          <button type='button' className='home-download'>Download Now</button>
+  return (
+    <div>
+      <Navbar />
+      <div className='homeinfo'>
+        <h1 className='home-main-title'>Meet banking that keeps pace with you</h1>
+        <h3 className='home-main-subtitle'>Open a fully free digital bank account and revamp the way you bank</h3>
+        <button type='button' className='home-download'>DOWNLOAD NOW</button>
+      </div>
+      <div className='home-body'>
+        <div className='home-body-head'>
+          <h1 className='home-nody-title text-center'>Your smart account is backed by</h1>
         </div>
-        <div className='home-body'>
-          <div className='home-body-head'>
-            <h1>Your smart account is backed by</h1>
-          </div>
-          <div className='home-features'>
-            <div className='home-feature'>
-              <h2>Max Trust</h2>
-              <p>We host your account in an RBI-licensed bank</p>
-              <img src='https://image.cnbcfm.com/api/v1/image/104240986-GettyImages-638057328.jpg?v=1529474008' alt='' width={100}></img>
-            </div>
-            <div className='home-feature'>
+        <div className='home-features'>
+          <div className='home-feature text-center'>
+            <img src={img1} alt='' width={100}></img>
             <h2>Max Trust</h2>
-              <p>We host your account in an RBI-licensed bank</p>
-              <img src='https://image.cnbcfm.com/api/v1/image/104240986-GettyImages-638057328.jpg?v=1529474008' alt=''  width={100}></img>
-            </div>
-            <div className='home-feature'>
-            <h2>Max Trust</h2>
-              <p>We host your account in an RBI-licensed bank</p>
-              <img src='https://image.cnbcfm.com/api/v1/image/104240986-GettyImages-638057328.jpg?v=1529474008' alt=''  width={100}></img>
-            </div>
+            <p>We host your account in an RBI-licensed bank</p>
+          </div>
+          <div className='home-feature text-center'>
+            <img src={img2} alt='' width={100}></img>
+            <h2>Max Security</h2>
+            <p>Bank grade security and ISO and PCI compliant</p>
+          </div>
+          <div className='home-feature text-center'>
+            <img src={img3} alt='' width={100}></img>
+            <h2>Max Safety</h2>
+            <p>Your money is insured upto &#8377;10,00,000</p>
           </div>
         </div>
-        <div>
-          <div></div>
-          <div></div>
-        </div>
-    </div>
-    )
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div >
+  )
 }
 
 export default Home
